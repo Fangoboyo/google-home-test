51254483ea7cdbdd82264221bcf84790a5124e9b
BongoCat <bungleberrysqueegybobber@gmail.com>
2024-01-03 21:19:31 -0800

    finished base product and dockerized

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use an official Rust runtime as a parent image
+FROM rust:1.54
+
+RUN rustup default nightly
+
+# Set the working directory in the container to /usr/src/GHBackend
+WORKDIR /usr/src/GHBackend
+
+# Copy the current directory contents into the container at /usr/src/GHBackend
+COPY . .
+# Install any needed packages specified in Cargo.toml
+RUN cargo install --path .
+
+# Make port 8000 available to the world outside this container
+EXPOSE 8000
+
+# Run the binary program produced by `cargo install`
+CMD ["GHBackend"]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use rocket::{get, http::Status, serde::json::Json};
 use serde::Serialize;
 
+use std::sync::Mutex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref PEOPLE_IN_ROOM: Mutex<i32> = Mutex::new(1);
+}
+
 #[macro_use]
 extern crate rocket;
 
@@ -12,24 +19,48 @@ pub struct GenericResponse {
 
 #[get("/exit")]
 pub async fn exit_handler() -> Result<Json<GenericResponse>, Status> {
-    const MESSAGE: &str = "Build Simple CRUD API with Rust and Rocket";
+    // Gets the number of people in the room from global variable
+    let mut people_in_room = PEOPLE_IN_ROOM.lock().unwrap();
+    *people_in_room -= 1;
 
-    let response_json = GenericResponse {
-        status: "success".to_string(),
-        message: MESSAGE.to_string(),
+    // Checks if there is no people in the room
+    let no_people_in_room: bool = *people_in_room < 1;
+    let message = if no_people_in_room {
+        // Google Home logic
+        "No people in room, lights successfully turned off".to_string()
+    } else {
+        format!("{} people in room", people_in_room)
     };
-    Ok(Json(response_json))
+
+    let response = GenericResponse {
+        status: "success".to_string(),
+        message,
+    };
+
+    Ok(Json(response))
 }
 
 #[get("/enter")]
 pub async fn enter_handler() -> Result<Json<GenericResponse>, Status> {
-    const MESSAGE: &str = "Build Simple CRUD API with Rust and Rocket";
+    // Gets the number of people in the room from global variable
+    let mut people_in_room = PEOPLE_IN_ROOM.lock().unwrap();
+    *people_in_room += 1;
 
-    let response_json = GenericResponse {
-        status: "success".to_string(),
-        message: MESSAGE.to_string(),
+    // Checks if there is no people in the room
+    let room_not_empty: bool = *people_in_room > 0;
+    let message = if room_not_empty {
+        // Google Home logic
+        "Room is no longer empty, Lights turned on".to_string()
+    } else {
+        "no one in room".to_string()
     };
-    Ok(Json(response_json))
+
+    let response = GenericResponse {
+        status: "success".to_string(),
+        message,
+    };
+
+    Ok(Json(response))
 }
 
 #[launch]
